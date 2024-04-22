4f9b7c95a9470fc7b32fd7c10e36b46ac11e49d9
Adrien Touzouli <contact@ad2ien.dev>
2024-04-22 09:22:26 +0200

    style: make linters happy

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -20,14 +20,14 @@ resource "openstack_lb_pool_v2" "api_pool" {
 }
 
 resource "openstack_lb_member_v2" "lb_member" {
-  count             = var.desired_nodes
-  name              = "lb-main-pool"
-  pool_id           = openstack_lb_pool_v2.api_pool.id
+  count   = var.desired_nodes
+  name    = "lb-main-pool"
+  pool_id = openstack_lb_pool_v2.api_pool.id
   # node private ip
-  address           = "192.168.40.200"
+  address = "192.168.40.200"
   # service node port (after ingress controller service creation)
-  protocol_port     = var.ingress_service_port
-  depends_on        = [ openstack_lb_pool_v2.api_pool ]
+  protocol_port = var.ingress_service_port
+  depends_on    = [openstack_lb_pool_v2.api_pool]
 }
 
 resource "openstack_lb_monitor_v2" "monitor" {
