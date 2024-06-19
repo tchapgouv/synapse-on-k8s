6a3b1bcd4ec720d369b49723f3b5c37f3715c6f5
Pipo Bimbo <heroinedor@gmail.com>
2024-06-19 14:44:00 +0200

    feat: health monitor everywhere and renaming of ressources

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,5 +1,5 @@
 resource "openstack_lb_loadbalancer_v2" "k8s_lb" {
-  name                  = "${var.env_name} load balancer"
+  name                  = "${var.env_name}-load-balancer"
   loadbalancer_provider = "amphora"
   vip_subnet_id         = openstack_networking_subnet_v2.subnet.id
   vip_address           = var.app_vlan_lb_ip
@@ -9,35 +9,44 @@ resource "openstack_lb_loadbalancer_v2" "k8s_lb" {
 }
 
 resource "openstack_lb_listener_v2" "websecure_listener" {
-  name            = "${var.env_name} websecure listener"
+  name            = "${var.env_name}-websecure-listener"
   protocol        = "HTTPS"
   protocol_port   = 443
   loadbalancer_id = openstack_lb_loadbalancer_v2.k8s_lb.id
 }
 
 resource "openstack_lb_listener_v2" "web_listener" {
-  name            = "${var.env_name} web listener"
+  name            = "${var.env_name}-web-listener"
   protocol        = "HTTP"
   protocol_port   = 80
   loadbalancer_id = openstack_lb_loadbalancer_v2.k8s_lb.id
 }
 
 resource "openstack_lb_pool_v2" "websecure_pool" {
-  name        = "${var.env_name} websecure pool"
+  name        = "${var.env_name}-k8s-websecure-pool"
   protocol    = "HTTPS"
   lb_method   = "ROUND_ROBIN"
   listener_id = openstack_lb_listener_v2.websecure_listener.id
 }
 
 resource "openstack_lb_pool_v2" "web_pool" {
-  name        = "${var.env_name} web pool"
+  name        = "${var.env_name}-k8s-web-pool"
   protocol    = "HTTP"
   lb_method   = "ROUND_ROBIN"
   listener_id = openstack_lb_listener_v2.web_listener.id
 }
 
-resource "openstack_lb_monitor_v2" "monitor" {
-  name        = "${var.env_name} Api Monitor"
+resource "openstack_lb_monitor_v2" "web_monitor" {
+  name        = "${var.env_name}-monitor-for-k8s-web-pool"
+  pool_id     = openstack_lb_pool_v2.web_pool.id
+  type        = "HTTP"
+  delay       = 10
+  timeout     = 5
+  max_retries = 5
+}
+
+resource "openstack_lb_monitor_v2" "websecure_monitor" {
+  name        = "${var.env_name}-monitor-for-k8s-websecure-pool"
   pool_id     = openstack_lb_pool_v2.websecure_pool.id
   type        = "HTTPS"
   delay       = 10
@@ -59,7 +68,7 @@ resource "openstack_networking_floatingip_associate_v2" "lb1" {
 
 resource "openstack_lb_member_v2" "lb_member_websecure" {
   count = length(local.nodes_ips)
-  name          = "websecure-member-${count.index}"
+  name          = "k8s-websecure-member-${count.index}"
   pool_id       = openstack_lb_pool_v2.websecure_pool.id
   address       = local.nodes_ips[count.index]
   protocol_port = var.ingress_service_port_websecure
@@ -68,7 +77,7 @@ resource "openstack_lb_member_v2" "lb_member_websecure" {
 
 resource "openstack_lb_member_v2" "lb_member_web" {
   count = length(local.nodes_ips)
-  name          = "web-member-${count.index}"
+  name          = "k8s-web-member-${count.index}"
   pool_id       = openstack_lb_pool_v2.web_pool.id
   address       = local.nodes_ips[count.index]
   protocol_port = var.ingress_service_port_web
@@ -79,7 +88,7 @@ resource "openstack_lb_member_v2" "lb_member_web" {
 
 resource "openstack_lb_pool_v2" "admin_websecure_pool" {
   count       = var.env_name != "production" ? 1 : 0
-  name        = "${var.env_name} admin websecure pool"
+  name        = "${var.env_name}-admin-websecure-pool"
   protocol    = "HTTPS"
   lb_method   = "ROUND_ROBIN"
   loadbalancer_id = openstack_lb_loadbalancer_v2.k8s_lb.id
@@ -87,25 +96,25 @@ resource "openstack_lb_pool_v2" "admin_websecure_pool" {
 
 resource "openstack_lb_pool_v2" "admin_web_pool" {
   count       = var.env_name != "production" ? 1 : 0
-  name        = "${var.env_name} admin web pool"
+  name        = "${var.env_name}-admin-web-pool"
   protocol    = "HTTP"
   lb_method   = "ROUND_ROBIN"
   loadbalancer_id = openstack_lb_loadbalancer_v2.k8s_lb.id
 }
 
 resource "openstack_lb_monitor_v2" "admin_websecure_pool_monitor" {
-  name        = "${var.env_name} Monitor for protocol TCP on port 443"
+  name        = "${var.env_name}-monitor-for-admin-websecure-pool"
   pool_id     = openstack_lb_pool_v2.admin_websecure_pool[0].id
-  type        = "TCP"
+  type        = "HTTPS"
   delay       = 10
   timeout     = 5
   max_retries = 5
 }
 
 resource "openstack_lb_monitor_v2" "admin_web_pool_monitor" {
-  name        = "${var.env_name} Monitor for protocol TCP on port 80"
+  name        = "${var.env_name}-monitor-for-admin-web-pool"
   pool_id     = openstack_lb_pool_v2.admin_web_pool[0].id
-  type        = "TCP"
+  type        = "HTTP"
   delay       = 10
   timeout     = 5
   max_retries = 5
@@ -131,7 +140,7 @@ resource "openstack_lb_l7policy_v2" "l7policy_admin" {
   count            = var.env_name != "production" ? 1 : 0
   name             = "${var.env_name} l7policy for admin VM"
   action           = "REDIRECT_TO_POOL"
-  description      = "test l7 policy"
+  description      = "redirect-to-admin-pool-policy"
   position         = 1
   listener_id      = openstack_lb_listener_v2.web_listener.id
   redirect_pool_id = openstack_lb_pool_v2.admin_web_pool[0].id
